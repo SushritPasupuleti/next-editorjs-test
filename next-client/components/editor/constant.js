7762e9bd3cceb0d880d93532c6f41b7a9d0d49c5
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-03-29 14:02:55 +0530

    set up proper endpoints for file upload (check express-s3 repo of mine for endpoint implementation)

--- next-client/components/editor/constant.js
+++ next-client/components/editor/constant.js
@@ -4,7 +4,7 @@ import List from "@editorjs/list";
 import Warning from "@editorjs/warning";
 import Code from "@editorjs/code";
 import LinkTool from "@editorjs/link";
-import Image from "@editorjs/image";
+import ImageE from "@editorjs/image";
 import Raw from "@editorjs/raw";
 import Header from "@editorjs/header";
 import Quote from "@editorjs/quote";
@@ -22,7 +22,15 @@ export const EDITOR_JS_TOOLS = {
   warning: Warning,
   code: Code,
   linkTool: LinkTool,
-  image: Image,
+  image: {
+      class: ImageE,
+      config: {
+        endpoints: {
+          byFile: 'http://localhost:5000/upload-img', // Your backend file uploader endpoint
+          byUrl: 'http://localhost:5000/fetchUrl', // Your endpoint that provides uploading by Url
+        }
+      }
+  },
   raw: Raw,
   header: Header,
   quote: Quote,
--- next-client/pages/index.js
+++ next-client/pages/index.js
@@ -13,6 +13,11 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.js</code>
+        </p>
       </main>
     </div>
   )
